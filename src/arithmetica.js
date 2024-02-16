cea6368e6b49e9a7bb9ecdc626ad01b10a08ec43
Owen Harris <harriowe@iu.edu>
2024-02-15 19:11:47 -0500

    Added arithmetica.js and added jest and nodemon to dev dependencies

---
+++ src/arithmetica.js
@@ -0,0 +1,17 @@
+function add(n, m) {
+    return n + m;
+}
+
+function subtract(n, m) {
+    return n - m
+}
+
+function multiply(n, m) {
+    return n * m;
+}
+
+function divide(n, m) {
+    return n / m;
+}
+
+module.exports = {add, subtract, multiply, divide}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3000;
+const { add, subtract, multiply, divide} = require("./arithmetica")
 
 app.use(cors());
 
@@ -10,19 +11,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/add/:n/:m', (req,res) => { // Fills request from any path /add/n/m where n and m are numbers
-  res.json(Number(req.params.n) + Number(req.params.m)); // Adds n and m as numbers and returns them as json
+  res.json(add(Number(req.params.n), Number(req.params.m))) // Adds n and m as numbers and returns them as json
 })
 
 app.get('/subtract/:n/:m', (req,res) => {
-  res.json(Number(req.params.n) - Number(req.params.m));
+  res.json(subtract(Number(req.params.n), Number(req.params.m)));
 })
 
 app.get('/multiply/:n/:m', (req,res) => {
-  res.json(Number(req.params.n) * Number(req.params.m));
+  res.json(multiply(Number(req.params.n), Number(req.params.m)));
 })
 
 app.get('/divide/:n/:m', (req,res) => {
-  res.json(Number(req.params.n) / Number(req.params.m));
+  res.json(divide(Number(req.params.n), Number(req.params.m)));
 })
 
 app.listen(port);
